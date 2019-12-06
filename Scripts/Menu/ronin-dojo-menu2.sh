f41d892a7252ad9fda6fb10d4aa31fa07238b12e
BTCxZelko <34781573+BTCxZelko@users.noreply.github.com>
2019-12-06 13:21:46 -0800

    Update ronin-dojo-menu2.sh

--- Scripts/Menu/ronin-dojo-menu2.sh
+++ Scripts/Menu/ronin-dojo-menu2.sh
@@ -11,10 +11,9 @@ CHOICE_HEIGHT=16
 TITLE="Ronin UI"
 MENU="Choose one of the following options:"
 
-OPTIONS=(1 "Enable Dojo at Startup"
-         2 "Disable Dojo at Startup"
-         3 "Uninstall Dojo"
-         4 "Go Back")
+OPTIONS=(1 "Clean Docker Images"
+         2 "Uninstall Dojo"
+         3 "Go Back")
 
 CHOICE=$(dialog --clear \
                 --title "$TITLE" \
@@ -28,86 +27,22 @@ case $CHOICE in
         1)
             echo -e "${RED}"
             echo "***"
-            echo "Creating executable start for service..."
+            echo "Deleting docker dangling images and images of previous versions in 15s..."
+            echo "Use Ctrl+C to exit if needed!"
             echo "***"
             echo -e "${NC}"
-            sleep 2s
-
-            USER=$(sudo cat /etc/passwd | grep 1000 | awk -F: '{ print $1}' | cut -c 1-)
-            echo ""
-            echo "
+            sleep 15s
             cd ~/dojo/docker/my-dojo/
-            sudo ./dojo.sh start
-            " | sudo tee -a /bin/dojo
-
-            sleep 1s
-            echo -e "${RED}"
-            echo "***"
-            echo "Making dojo.service file..."
-            echo "***"
-            echo -e "${NC}"
-            sleep 2s
-
-            echo "
-            [Unit]
-            Description=Dojo
-            After=docker.service
-            [Service]
-            WorkingDirectory=/home/$USER/dojo/docker/my-dojo
-            ExecStart=/bin/dojo
-            User=root
-            Group=root
-            Type=simple
-            KillMode=process
-            TimeoutSec=60
-            Restart=always
-            RestartSec=60
-            [Install]
-            WantedBy=multi-user.target
-            " | sudo tee -a /etc/systemd/system/dojo.service
-
-            sudo systemctl enable dojo
-            # enables dojo system service
-
-            echo -e "${RED}"
-            echo "***"
-            echo "Dojo.service is enabled and will launch at system start up."
-            echo "***"
-            echo -e "${NC}"
-            sleep 5s
-            bash ~/RoninDojo/Scripts/Menu/ronin-dojo-menu2.sh
-            # start dojo, return to menu
+            sudo ./dojo.sh clean
+            # free disk space by deleting docker dangling images and images of previous versions
             ;; 
         2)
-            echo -e "${RED}"
-            echo "***"
-            echo "Disable Dojo At Startup..."
-            echo "***"
-            echo -e "${NC}"
-            sudo systemctl disable dojo
-            sleep 2s
-            
-            sudo rm /etc/systemd/system/dojo.service
-            sudo rm /bin/dojo
-            sleep 2s
-            
-            echo -e "${RED}"
-            echo "***"
-            echo "Dojo.service is enabled and will launch at system start up."
-            echo "***"
-            echo -e "${NC}"
-            sleep 5s
-            bash ~/RoninDojo/Scripts/Menu/ronin-dojo-menu2.sh
-            # start dojo, return to menu
-            ;;
-        3)
             echo -e "${RED}"
             echo "***"
             echo "Uninstalling Dojo in 30s..."
             echo "***"
             echo -e "${NC}"
             sleep 5s
-
             echo -e "${RED}"
             echo "***"
             echo "WARNING: This will uninstall Dojo, use Ctrl+C to exit if needed!"
@@ -118,7 +53,7 @@ case $CHOICE in
             sudo ./dojo.sh uninstall
             # uninstall dojo
             ;;
-        4)
+        3)
             bash ~/RoninDojo/Scripts/Menu/ronin-dojo-menu.sh
             # return to main menu
             ;;
