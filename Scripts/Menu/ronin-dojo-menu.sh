25abd9a3dfb703a3fc04219f09144adb145f3a13
BTCxZelko <34781573+BTCxZelko@users.noreply.github.com>
2019-12-07 19:38:20 -0800

    Update ronin-dojo-menu.sh

--- Scripts/Menu/ronin-dojo-menu.sh
+++ Scripts/Menu/ronin-dojo-menu.sh
@@ -13,9 +13,9 @@ MENU="Choose one of the following options:"
 
 OPTIONS=(1 "Start Dojo"
          2 "Stop Dojo"
-         3 "View Logs"
-         4 "Tor Hidden Service Address"
-         5 "Upgrade Dojo"
+         3 "Restart Dojo"
+         4 "View Logs"
+         5 "Tor Hidden Service Address"
          6 "Version Info"
          7 "IBD Transfer"
          8 "Next Page"
@@ -56,7 +56,20 @@ case $CHOICE in
 	    bash ~/RoninDojo/Scripts/Menu/ronin-dojo-menu.sh
             # stop dojo, return to menu
             ;;
-        3)
+	 3)
+            echo -e "${RED}"
+            echo "***"
+            echo "Restarting Dojo in 5s..."
+            echo "Use Ctrl+C to exit if needed!"
+            echo "***"
+            echo -e "${NC}"
+            sleep 5s
+	    cd ~/dojo/docker/my-dojo
+	    sudo ./dojo.sh restart
+            bash ~/RoninDojo/Scripts/Menu/ronin-dojo-menu.sh
+            # upgrades dojo
+            ;;
+        4)
             echo -e "${RED}"
             echo "***"
             echo "Type one of the following options now: (bitcoind, db, tor, api, tracker, pushtx, pushtx-orchest)"
@@ -77,7 +90,7 @@ case $CHOICE in
             bash ~/RoninDojo/Scripts/Menu/ronin-dojo-menu.sh
             # return to main menu
             ;;
-        4)
+        5)
             echo -e "${RED}"
             echo "***"
             echo "Showing the Tor Hidden Service Address."
@@ -98,36 +111,6 @@ case $CHOICE in
             # press any key to return to menu
             # shows .onion and returns to menu
             ;;
-        5)
-            echo -e "${RED}"
-            echo "***"
-            echo "Upgrading Dojo in 30s..."
-            echo "Use Ctrl+C to exit if needed!"
-            echo "***"
-            echo -e "${NC}"
-            sleep 30s
-	    cd ~/dojo/docker/my-dojo
-	    sudo ./dojo.sh stop
-	    mkdir ~/.dojo > /dev/null 2>&1
-	    cd ~/.dojo
-	    git clone -b master https://github.com/Samourai-Wallet/samourai-dojo.git
-	    sudo cp -rv samourai-dojo/* ~/dojo
-	    sed -i '9d' ~/dojo/docker/my-dojo/bitcoin/Dockerfile
-	    sed -i '9i             ENV     BITCOIN_URL         https://bitcoincore.org/bin/bitcoin-core-0.19.0.1/bitcoin-0.19.0.1-aarch64-linux-gnu.tar.gz' ~/dojo/docker/my-dojo/bitcoin/Dockerfile
-	    sed -i '10d' ~/dojo/docker/my-dojo/bitcoin/Dockerfile
-	    sed -i '10i            ENV     BITCOIN_SHA256      c258c6416225afb08c4396847eb3d5da61a124f1b5c61cccb5a2e903e453ce7f' ~/dojo/docker/my-dojo/bitcoin/Dockerfile
-	    sed -i '1d' ~/dojo/docker/my-dojo/mysql/Dockerfile
-	    sed -i '1i             FROM    mariadb:latest' ~/dojo/docker/my-dojo/mysql/Dockerfile
-	    sed -i '12d' ~/dojo/docker/my-dojo/tor/Dockerfile
-	    sed -i '12i ENV     GOLANG_ARCHIVE      go1.13.5.linux-arm64.tar.gz' ~/dojo/docker/my-dojo/tor/Dockerfile
-	    sed -i '13d' ~/dojo/docker/my-dojo/tor/Dockerfile
-	    sed -i '13i ENV     GOLANG_SHA256       227b718923e20c846460bbecddde9cb86bad73acc5fb6f8e1a96b81b5c84668b' ~/dojo/docker/my-dojo/tor/Dockerfile
-            cd ~/dojo/docker/my-dojo/	   
-            sleep 2s
-	    sudo ./dojo.sh upgrade
-            bash ~/RoninDojo/Scripts/Menu/ronin-dojo-menu.sh
-            # upgrades dojo
-            ;;
         6)
             echo -e "${RED}"
             echo "***"
@@ -136,7 +119,8 @@ case $CHOICE in
             echo -e "${NC}"
             sleep 3s
             cd ~/dojo/docker/my-dojo/
-            sudo ./dojo.sh version            
+            sudo ./dojo.sh version
+            
             echo -e "${RED}"
             echo "***"
             echo "Press any letter to return..."
